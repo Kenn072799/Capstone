5cb242b20e287e5b8d0ea6b00dd5e96c6d65a0e2
Kenneth Altes <kennethaltes27@gmail.com>
2024-08-04 20:19:58 +0800

    update: main

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './pages/Home';
+import Projectpage from './pages/Projectpage';
+import AllTestimonialpage from './pages/AllTestimonialpage';
+
+const router = createBrowserRouter([
+  {
+    path: "/Mrquickfix/",
+    element: <Home />,
+  },
+  {
+    path: "/projects/all/",
+    element: <Projectpage />,
+  },
+  {
+    path: "/testimonials/all/",
+    element: <AllTestimonialpage />,
+  },
+]);
 
 const App = () => {
   return (
-    <div>App</div>
-  )
+    <RouterProvider router={router} />
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,31 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-
-import Home from "./pages/Home";
-import Projectpage from "./pages/Projectpage";
-import AllTestimonialpage from "./pages/AllTestimonialpage";
-
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
-const router = createBrowserRouter([
-  {
-    path: "/Mrquickfix/",
-    element: <Home />,
-    
-  },
-  {
-    path: "/projects/all/",
-    element: <Projectpage />,
-  },
-  {
-    path: "/testimonials/all/",
-    element: <AllTestimonialpage />,
-  },
-]);
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
